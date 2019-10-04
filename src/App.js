367c4a88fefa4d9dfb5ec72909edbfec62d9329c
Maximilien Garenne <maximilien.garenne@gmail.com>
2019-10-05 00:07:54 +0200

    [master] add util to create runkit content from js code and give first fetch example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
+import Embed from 'react-runkit'
+
+import createRunkitContentFromJSCode from './createRunkitContentFromJSCode'
+ 
+
 import './App.css';
+
+
+// redefine require so it can be used in code preparation.
+
+// Several constraints to make it work :
+// 1) redefine require so it can be used in code preparation.
+const require = () => { }
+// 2) store code you wanna use in a function declared as a named function, so toString method keeps its name, thus it can be called automatically.
+// if it was a anonymous function stored in a variable, the toString method render 
+
+
+function testo() {
+
+  var fetch = require("cross-fetch")
+
+
+
+  fetch('https://opentdb.com/api.php?amount=10#').then((res)=> res.json()).then((data)=> console.log(data))
+}
+
+
+
+const url ='https://runkit.com/hyphaene/5d97845fd8b6190014638429';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="embedWrapper" >
+          <Embed source={ createRunkitContentFromJSCode(testo) } />
+    
+    </div>
       </header>
     </div>
   );
 }
-
+  
 export default App;
---
+++ src/createRunkitContentFromJSCode.js
@@ -0,0 +1,28 @@
+// redefine require so it can be used in code preparation.
+
+// Several constraints to make it work :
+// 1) redefine require so it can be used in code preparation.
+// const require = () => {};
+// 2) store all the code you wanna include in a function which will be passed as an arg for the createRunkit function
+
+const transformFunctionAsStripedArray = func =>
+  func
+    .toString()
+    .split('\n')
+    .filter(line => !!line.trim()).map((line)=> line.trim());
+
+
+
+const getArrayWithFirstAndLastElementRemoved = array => array.slice(1, array.length -1)
+
+const convertArrayBackToString = array => array.join('\n')
+
+const createRunkitContentFromJSCode = func =>
+  convertArrayBackToString(
+    getArrayWithFirstAndLastElementRemoved(
+      transformFunctionAsStripedArray(func)
+    )
+  );
+
+
+export default createRunkitContentFromJSCode;
