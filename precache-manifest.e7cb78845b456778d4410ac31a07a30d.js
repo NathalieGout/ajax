d7d51556f01a665be1db605a18b38938c7d74370
Maximilien Garenne <maximilien.garenne@gmail.com>
2019-10-04 10:29:35 +0200

    Updates

---
+++ precache-manifest.e7cb78845b456778d4410ac31a07a30d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f131c6514b51542f759372a5227815b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed8472ae7b5ad0ee0f8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "61abd2abe72a771a69d2",
+    "url": "/static/js/2.170eb419.chunk.js"
+  },
+  {
+    "revision": "1ed8472ae7b5ad0ee0f8",
+    "url": "/static/js/main.a10fac5d.chunk.js"
+  },
+  {
+    "revision": "3cf11af896e4ce361d06",
+    "url": "/static/js/runtime-main.ebc252d5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
