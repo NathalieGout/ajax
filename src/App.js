56f3baef4458463c89b1d1b4e192b3d6968ff434
Maximilien Garenne <maximilien.garenne@gmail.com>
2019-10-07 16:59:09 +0200

    [master] replace local createRunkit .. by pkg

--- src/App.js
+++ src/App.js
@@ -1,47 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import Embed from 'react-runkit'
+import Embed from 'react-runkit';
 
-import createRunkitContentFromJSCode from './createRunkitContentFromJSCode'
- 
+import createRunkitContentFromJSFunction from 'create-runkit-content-from-function';
 
 import './App.css';
-
 
 // redefine require so it can be used in code preparation.
 
 // Several constraints to make it work :
 // 1) redefine require so it can be used in code preparation.
-const require = () => { }
+const require = () => {};
 // 2) store code you wanna use in a function declared as a named function, so toString method keeps its name, thus it can be called automatically.
-// if it was a anonymous function stored in a variable, the toString method render 
-
+// if it was a anonymous function stored in a variable, the toString method render
 
 function testo() {
+	var fetch = require('cross-fetch');
 
-  var fetch = require("cross-fetch")
-
-
-
-  fetch('https://opentdb.com/api.php?amount=10#').then((res)=> res.json()).then((data)=> console.log(data))
+	fetch('https://opentdb.com/api.php?amount=10#')
+		.then(res => res.json())
+		.then(data => console.log(data));
 }
 
+function tt() {
+	var fetch = require('cross-fetch');
 
+	const asyncFunc = async () => {
+		const res = await fetch('https://opentdb.com/api.php?amount=10#');
+		const data = await res.json();
+		console.log(data);
+	};
 
-const url ='https://runkit.com/hyphaene/5d97845fd8b6190014638429';
+	asyncFunc();
+}
+
+const url = 'https://runkit.com/hyphaene/5d97845fd8b6190014638429';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="embedWrapper" >
-          <Embed source={ createRunkitContentFromJSCode(testo) } />
-    check  
-    <a href="https://github.com/jlengstorf/gatsby-with-apollo">https://github.com/jlengstorf/gatsby-with-apollo</a>
-    </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div className="embedWrapper">
+					<Embed source={createRunkitContentFromJSFunction(testo)} />
+					<Embed source={createRunkitContentFromJSFunction(tt)} />
+					check
+					<a href="https://github.com/jlengstorf/gatsby-with-apollo">
+						https://github.com/jlengstorf/gatsby-with-apollo
+					</a>
+				</div>
+			</header>
+		</div>
+	);
 }
-  
+
 export default App;
